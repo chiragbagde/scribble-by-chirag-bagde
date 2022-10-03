11fb84ab2672ab07448b1279606855835261f5ca
Chirag bagde <chiragsbagde@gmail.com>
2022-10-03 12:47:09 +0530

    Setup tailwindcss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,8 @@
-import "../stylesheets/application.scss";
-
 require("@rails/ujs").start();
 require("@rails/activestorage").start();
 require("channels");
+
+import "../stylesheets/application.scss";
 
 var componentRequireContext = require.context("src", true);
 var ReactRailsUJS = require("react_ujs");
