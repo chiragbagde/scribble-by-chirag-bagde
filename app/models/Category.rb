387288df9f5a32a502df9a4990632e92e7f43eff
Chirag Bagde <chirag.bagde@bigbinary.com>
2022-10-29 03:08:09 +0530

    changes (#17)

--- app/models/Category.rb
+++ app/models/Category.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
-  include Filterable
   belongs_to :assigned_organisation, foreign_key: :assigned_organisation_id, class_name: "Organisation"
   has_many :assigned_articles, foreign_key: :assigned_category_id, class_name: "Article"
 
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationRecord < ActiveRecord::Base
+  include Filterable
   include ActionView::Helpers::TranslationHelper
   self.abstract_class = true
 
