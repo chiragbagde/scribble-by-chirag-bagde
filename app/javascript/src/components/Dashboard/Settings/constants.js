0edb3fd958efdf071a8deb23b8afd7b5bc6a4990
Chirag bagde <chiragsbagde@gmail.com>
2022-11-04 02:46:32 +0530

    updated constants

--- app/javascript/src/components/Dashboard/Settings/constants.js
+++ app/javascript/src/components/Dashboard/Settings/constants.js
@@ -1,21 +1,18 @@
 export const MENU_ITEMS = [
   {
     label: "General",
-    url: "/settings/",
     value: "general",
     description: "Page Title, Brand Name & Meta Description",
     id: 1,
   },
   {
     label: "Redirections",
-    url: "/settings/redirections",
     value: "redirections",
     description: "Create & configure redirection rules",
     id: 2,
   },
   {
     label: "Manage Categories",
-    url: "/settings/managecategories",
     value: "managecategories",
     description: "Edit and Reorder KB Structure",
     id: 3,
